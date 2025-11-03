23afb1e165f9d8cf2481c3095c8f3d2d2d5a849c
Abdul-rehman-rgb <abdul4758rehman@gmail.com>
2025-11-03 22:04:47 +0500

    added

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(helmet());
 app.use(
   cors(
     {
-    origin: ["http://localhost:3000", "https://infinitum-alpha.vercel.app"],
+    origin: ["https://infinitum-alpha.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, 
