bfb5f657ffe790b925ea40a69a2a6d0131fd8f24
Abdul-rehman-rgb <abdul4758rehman@gmail.com>
2025-11-03 21:38:36 +0500

    added

--- index.js
+++ index.js
@@ -25,12 +25,14 @@ import database from "./config/database.js";
 database.connect();
 const app = express();
 
+// https://infinitum-alpha.vercel.app
+
 // Security middleware
 app.use(helmet());
 app.use(
   cors(
     {
-    origin: ["https://infinitum-alpha.vercel.app"],
+    origin: ["http://localhost:3000", "https://infinitum-alpha.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, 
@@ -79,6 +81,6 @@ app.use("/api/projects", projectRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
-  console.log(`🌍 Environment: ${process.env.NODE_ENV || "development"}`);
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
 });
